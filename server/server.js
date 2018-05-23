b05a8ec09baf0cbb4b3f9339e212e59a9b7954b4
Alex Budaev <budaevaa@mail.ru>
2018-05-23 22:05:18 +0700

    create server and config

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log('start server');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DATABASE);
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
+
+
