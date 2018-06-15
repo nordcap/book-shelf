acac46f790ec12159c26c73632b485b64e1fa4a2
Alex Budaev <budaevaa@mail.ru>
2018-06-16 02:03:24 +0700

    load book in list books

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,28 +1,53 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-export function getBooks(
-    limit = 10,
-    start = 0,
-    order = 'asc',
-    list = ''
-) {
+export function getBooks(limit = 10, start = 0, order = "asc", list = "") {
     //localhost:3001/api/books?skip=3&limit=2&order=asc
     //send request
-    const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
+    const request = axios
+        .get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
         .then(response => {
             if (list) {
-                return [...list, ...response.data]
+                return [...list, ...response.data];
             } else {
-                return response.data
+                return response.data;
             }
         });
 
-
     return {
-        type: 'GET_BOOKS',
+        type: "GET_BOOKS",
         payload: request
-    }
-
+    };
 }
 
+export function getBookWithReviewer(id) {
+    const request = axios.get(`/api/getBook?id=${id}`);
+
+    return dispatch => {
+        request.then(({ data }) => {
+            let book = data;
+            axios
+                .get(`/api/getReviewer?id=${book.ownerId}`)
+                .then(({ data }) => {
+                    let response = {
+                        book,
+                        reviewer: data
+                    };
+
+                    dispatch({
+                        type: "GET_BOOK_W_REVIEWER",
+                        payload: response
+                    });
+                });
+        });
+    };
+}
+
+export function clearBookWithReviewer() {
+    return {
+        type: "CLEAR_BOOK_W_REVIEWER",
+        payload: {
+            book: {},
+            reviewer: {}
+        }
+    };
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-
-import Layout from './hoc/layout';
-import Home from './components/Home/home';
+import Layout from "./hoc/layout";
+import Home from "./components/Home/home";
+import BookView from "./components/Books";
 
 const Routes = () => {
-    return (
-        <Layout>
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/books/:id" exact component={BookView} />
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
