abb2ddbf93faf95103fdaefd6921d3f4bd19b3f4
Alex Budaev <budaevaa@mail.ru>
2018-06-30 11:37:56 +0700

    fix logout - delete warning

--- client/src/components/Admin/logout.js
+++ client/src/components/Admin/logout.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const Logout = props => {
-    let request = axios.get(`/api/logout`).then(request => {
+    axios.get(`/api/logout`).then(request => {
         setTimeout(() => {
             props.history.push("/"); //переадресация на дом.страницу
         }, 2000);
