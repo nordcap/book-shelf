8a3837a24b3c9be4f2ccdffdf2f40e3c27128ed6
Alex Budaev <budaevaa@mail.ru>
2018-06-30 12:38:35 +0700

    filter for navitems (auth)

--- client/src/components/Header/Sidenav/sidenav_items.js
+++ client/src/components/Header/Sidenav/sidenav_items.js
@@ -1,60 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import FontAwesome from 'react-fontawesome';
+import React from "react";
+import { Link } from "react-router-dom";
+import FontAwesome from "react-fontawesome";
+import { connect } from "react-redux";
 
-const SidenavItems = () => {
-
+const SidenavItems = ({ user }) => {
     const items = [
         {
-            type: 'navItem',
-            icon: 'home',
-            text: 'Главная',
-            link: '/',
+            type: "navItem",
+            icon: "home",
+            text: "Главная",
+            link: "/",
             restricted: false
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Профиль',
-            link: '/user',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Профиль",
+            link: "/user",
+            restricted: true
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Добавить админа',
-            link: '/user/register',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Добавить админа",
+            link: "/user/register",
+            restricted: true
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Вход',
-            link: '/login',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Вход",
+            link: "/login",
+            restricted: false,
+            exclude: true
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Отзывы',
-            link: '/user/user-reviews',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Мои отзывы",
+            link: "/user/user-reviews",
+            restricted: true
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Добавить отзыв',
-            link: '/user/add',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Добавить отзыв",
+            link: "/user/add",
+            restricted: true
         },
         {
-            type: 'navItem',
-            icon: 'file-text-o',
-            text: 'Выход',
-            link: '/user/logout',
-            restricted: false
+            type: "navItem",
+            icon: "file-text-o",
+            text: "Выход",
+            link: "/user/logout",
+            restricted: true
         }
-    ]
+    ];
 
     const element = (item, i) => (
         <div key={i} className={item.type}>
@@ -63,19 +64,28 @@ const SidenavItems = () => {
                 {item.text}
             </Link>
         </div>
-    )
-
-    const showItems = () => (
-        items.map((item, i) => {
-            return element(item, i)
-        })
-    )
-
-    return (
-        <div>
-            {showItems()}
-        </div>
     );
+
+    const showItems = () =>
+        user.login
+            ? items.map((item, i) => {
+                  if (user.login.isAuth) {
+                      //если пользователь зарегистрирован, то вернет все элементы кроме "/login",
+                      return !item.exclude ? element(item, i) : null;
+                  } else {
+                      //если пользователь не зарегистрирован, вернет пункты с restricted=false Главная и Вход
+                      return !item.restricted ? element(item, i) : null;
+                  }
+              })
+            : null;
+
+    return <div>{showItems()}</div>;
 };
 
-export default SidenavItems;
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    };
+}
+
+export default connect(mapStateToProps)(SidenavItems);
